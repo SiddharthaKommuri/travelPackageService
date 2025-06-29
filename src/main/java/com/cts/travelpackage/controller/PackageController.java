42e15c4b39782a7d6985c00d4fc38a2857dac5e6
Kommuri <2408366@cognizant.com>
2025-06-29 17:48:31 +0530

    changes done to pom removed sec congig

--- src/main/java/com/cts/travelpackage/controller/PackageController.java
+++ src/main/java/com/cts/travelpackage/controller/PackageController.java
@@ -2,14 +2,7 @@ package com.cts.travelpackage.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.cts.travelpackage.dto.TravelPackageDto;
 import com.cts.travelpackage.dto.TravelPackageResponse;
@@ -22,6 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("api/v1/packages")
 @Slf4j
+@CrossOrigin
 public class PackageController {
 
 	private PackageService packageService;
